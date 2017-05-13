fc8e478634971b02e60b22c413e33603550bd6ca
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2017-05-13 13:08:35 +0200

    Write tests for description logic

---
+++ InfixCalculatorTests/CalculatorBrainTest.swift
@@ -0,0 +1,160 @@
+import XCTest
+@testable import InfixCalculator
+
+class CalculatorBrainTest: XCTestCase {
+
+    var brain: CalculatorBrain = CalculatorBrain()
+
+    typealias Expectation = (value: Double, description: String)
+    let accuracy: Double = 0.000000000001
+
+    override func setUp() {
+        super.setUp()
+        brain = CalculatorBrain()
+    }
+
+    // Task 7a
+    func testMissingOperand() {
+        let expected: Expectation = (value: 7, description: "7 + …")
+
+        brain.setOperand(7)
+        brain.performOperation("+")
+
+        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
+        XCTAssertEqual(expected.description, brain.description)
+    }
+
+    // Task 7b
+    func testPendingOperation() {
+        let expected: Expectation = (value: 9, description: "7 + …")
+
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+
+        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
+        XCTAssertEqual(expected.description, brain.description)
+    }
+
+    // Task 7c
+    func testCompletedOperation() {
+        let expected: Expectation = (value: 16, description: "7 + 9 =")
+
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("=")
+
+        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
+        XCTAssertEqual(expected.description, brain.description)
+    }
+
+    // Task 7d
+    func testBinaryWithinUnaryOperation() {
+        let expected: Expectation = (value: 4, description: "√(7 + 9) =")
+
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("=")
+        brain.performOperation("√")
+
+        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
+        XCTAssertEqual(expected.description, brain.description)
+    }
+
+    // Task 7e
+    func testBinaryWithinUnaryFollowedByBinaryOperation() {
+        let expected: Expectation = (value: 6, description: "√(7 + 9) + 2 =")
+
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("=")
+        brain.performOperation("√")
+        brain.performOperation("+")
+        brain.setOperand(2)
+        brain.performOperation("=")
+
+        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
+        XCTAssertEqual(expected.description, brain.description)
+    }
+
+    // Task 7f
+    func testUnaryWithinPendingBinaryOperation() {
+        let expected: Expectation = (value: 3, description: "7 + √(9)…")
+
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("√")
+
+        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
+        XCTAssertEqual(expected.description, brain.description)
+    }
+
+    // Task 7g
+    func testUnaryWithinBinaryOperation() {
+        let expected: Expectation = (value: 10, description: "7 + √(9) =")
+
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("√")
+        brain.performOperation("=")
+
+        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
+        XCTAssertEqual(expected.description, brain.description)
+    }
+
+    // Task 7h
+    func testMultipleSequentialBinaryOperations() {
+        let expected: Expectation = (value: 25, description: "7 + 9 + 6 + 3 =")
+
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("=")
+        brain.performOperation("+")
+        brain.setOperand(6)
+        brain.performOperation("=")
+        brain.performOperation("+")
+        brain.setOperand(3)
+        brain.performOperation("=")
+
+        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
+        XCTAssertEqual(expected.description, brain.description)
+    }
+
+    // Task 7i
+    func testStartNewDescriptionAfterCompletedOperation() {
+        let expected: Expectation = (value: 9, description: "6 + 3 =")
+
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("√")
+        brain.setOperand(6)
+        brain.performOperation("+")
+        brain.setOperand(3)
+        brain.performOperation("=")
+
+        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
+        XCTAssertEqual(expected.description, brain.description)
+    }
+
+    // Task 7j is tested in InfixCalculatorUITests.swift
+
+    // Task 7k
+    func testBinaryOperationWithConstant() {
+        let expected: Expectation = (value: 12.5663706143592, description: "4 × π =")
+
+        brain.setOperand(4)
+        brain.performOperation("×")
+        brain.performOperation("π")
+        brain.performOperation("=")
+
+        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
+        XCTAssertEqual(expected.description, brain.description)
+    }
+}
--- InfixCalculatorUITests/InfixCalculatorUITests.swift
+++ InfixCalculatorUITests/InfixCalculatorUITests.swift
@@ -1,36 +1,29 @@
-//
-//  InfixCalculatorUITests.swift
-//  InfixCalculatorUITests
-//
-//  Created by Hans van Luttikhuizen on 26/03/2017.
-//  Copyright © 2017 Hans van Luttikhuizen. All rights reserved.
-//
-
 import XCTest
 
 class InfixCalculatorUITests: XCTestCase {
-        
+
+    var app: XCUIApplication = XCUIApplication()
+
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app = XCUIApplication()
+        app.launch()
+    }
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    // Task 7j
+    func testUnaryOperation() {
+        app.buttons["5"].tap()
+        app.buttons["+"].tap()
+        app.buttons["6"].tap()
+        app.buttons["="].tap()
+        app.buttons["7"].tap()
+        app.buttons["3"].tap()
+
+        let expectedResult = app.staticTexts["73"]
+        let expectedHistory = app.staticTexts["5 + 6 ="]
+
+        XCTAssert(expectedResult.exists, "Main display should show entered value")
+        XCTAssert(expectedHistory.exists, "Description display should show most recent state")
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
 }
