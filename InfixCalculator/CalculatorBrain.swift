2b8dc7ba71051231316d45dcd5d58bdd4f2280df
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2017-06-28 19:25:45 +0200

    Switch to CalculatorBrain to evaluate model

--- InfixCalculator/CalculatorBrain.swift
+++ InfixCalculator/CalculatorBrain.swift
@@ -1,9 +1,11 @@
 import Foundation
 
 struct CalculatorBrain {
-
-    private var accumulator: (value: Double?, description: String) = (nil, "")
-    private var pendingBinaryOperation: PendingBinaryOperation?
+    private enum Element {
+        case operand(Double)
+        case operation(Operation)
+        case variable(String)
+    }
 
     private enum Operation {
         case constant(Double, String)
@@ -15,6 +17,8 @@ struct CalculatorBrain {
     private enum Precedence: Int {
         case min = 0, max
     }
+
+    private var elements: [Element] = []
 
     // @formatter:off
     private var operations: Dictionary<String, Operation> = [
@@ -31,16 +35,67 @@ struct CalculatorBrain {
     ]
     // @formatter:on
 
-    mutating func setOperand(_ operand: Double) {
-        if !operationPending {
-            accumulator.description = ""
-        }
-
-        accumulator.value = operand
-        accumulator.description += operand.display
+    mutating func setOperand(_ value: Double) {
+        elements.append(.operand(value))
     }
 
-        if let operation = operation[symbol] {
+    mutating func setOperand(_ symbol: String) {
+        elements.append(.variable(symbol))
+    }
+
+    mutating func setOperation(_ symbol: String) {
+        let operation = operations[symbol]
+
+        guard operation != nil else {
+            return // TODO Throw error
+        }
+
+        elements.append(.operation(operation!))
+    }
+
+    mutating func clear() {
+        elements = []
+    }
+
+    struct PendingBinaryOperation {
+        let firstOperand: Double
+        let currentDescription: String
+        let operationFunction: (Double, Double) -> Double
+        let descriptionFunction: (String, String) -> String
+
+        func perform(with secondOperand: Double) -> Double {
+            return operationFunction(firstOperand, secondOperand)
+        }
+
+        func describe(with secondOperand: String) -> String {
+            return descriptionFunction(currentDescription, secondOperand)
+        }
+    }
+
+    func evaluate(using variables: Dictionary<String, Double>? = nil) -> (result: Double?, isPending: Bool, description: String) {
+        guard !elements.isEmpty else {
+            return (nil, false, "")
+        }
+
+        var accumulator: (value: Double?, description: String) = (nil, "")
+        var pendingBinaryOperation: PendingBinaryOperation?
+        var currentPrecedence = Precedence.max
+        var operationPending: Bool {
+            return pendingBinaryOperation != nil
+        }
+
+        func setOperand(value: Double) {
+            accumulator.value = value
+            accumulator.description = value.display
+            currentPrecedence = Precedence.max
+        }
+
+        func setOperand(symbol: String) {
+            accumulator.value = variables?[symbol] ?? 0
+            accumulator.description = symbol
+            currentPrecedence = Precedence.max
+        }
+
         func setOperation(_ operation: Operation) {
             switch operation {
             case .constant(let value, let description):
@@ -63,31 +118,38 @@ struct CalculatorBrain {
                 performPendingBinaryOperation()
             }
         }
-    }
-
-    private mutating func performPendingBinaryOperation() {
-        if operationPending && accumulator.value != nil {
-            let result = pendingBinaryOperation!.perform(with: accumulator.value!)
-            let description = pendingBinaryOperation!.describe(with: accumulator.description)
-
-            accumulator = (result, "\(description)")
-            pendingBinaryOperation = nil
+        
+        func performPendingBinaryOperation() {
+            guard operationPending else {
+                return
+            }
+            
+            if accumulator.value != nil {
+                let result = pendingBinaryOperation!.perform(with: accumulator.value!)
+                let description = pendingBinaryOperation!.describe(with: accumulator.description)
+                
+                accumulator = (result, description)
+                pendingBinaryOperation = nil
+            } // TODO Else?
         }
-    }
-
-    private struct PendingBinaryOperation {
-        let firstOperand: Double
-        let currentDescription: String
-        let operation: (Double, Double) -> Double
-        let description: (String, String) -> String
-
-        func perform(with secondOperand: Double) -> Double {
-            return operation(firstOperand, secondOperand)
         }
 
-        func describe(with secondOperand: String) -> String {
-            return description(currentDescription, secondOperand)
+        for element in elements {
+            switch element {
+            case .operand(let operand):
+                setOperand(value: operand)
+            case .operation(let operation):
+                setOperation(operation)
+            case .variable(let variable):
+                setOperand(symbol: variable)
+            }
         }
+        
+        return (
+                result: accumulator.value,
+                isPending: operationPending,
+                description: accumulator.description
+        )
     }
 
     mutating func clear() {
--- InfixCalculatorTests/CalculatorBrainTest.swift
+++ InfixCalculatorTests/CalculatorBrainTest.swift
@@ -2,9 +2,7 @@ import XCTest
 @testable import InfixCalculator
 
 class CalculatorBrainTest: XCTestCase {
-
     var brain: CalculatorBrain = CalculatorBrain()
-
     typealias Expectation = (value: Double, description: String)
     let accuracy: Double = 0.000000000001
 
@@ -15,58 +13,60 @@ class CalculatorBrainTest: XCTestCase {
 
     // Task 7a
     func testMissingOperand() {
-        let expected: Expectation = (value: 7, description: "7 + ")
-
         brain.setOperand(7)
         brain.setOperation("+")
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        let expected: Expectation = (value: 7, description: "7 + ")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
     }
 
     // Task 7b
     func testPendingOperation() {
-        let expected: Expectation = (value: 9, description: "7 + 9")
-
         brain.setOperand(7)
         brain.setOperation("+")
         brain.setOperand(9)
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        let expected: Expectation = (value: 9, description: "7 + 9")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
     }
 
     // Task 7c
     func testCompletedOperation() {
         let expected: Expectation = (value: 16, description: "7 + 9")
+        let actual = brain.evaluate()
 
         brain.setOperand(7)
         brain.setOperation("+")
         brain.setOperand(9)
         brain.setOperation("=")
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        XCTAssertEqualWithAccuracy(brain.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(brain.description, expected.description)
     }
 
     // Task 7d
     func testBinaryWithinUnaryOperation() {
-        let expected: Expectation = (value: 4, description: "√(7 + 9)")
-
         brain.setOperand(7)
         brain.setOperation("+")
         brain.setOperand(9)
         brain.setOperation("=")
         brain.setOperation("√")
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        let expected: Expectation = (value: 4, description: "√(7 + 9)")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
     }
 
     // Task 7e
     func testBinaryWithinUnaryFollowedByBinaryOperation() {
-        let expected: Expectation = (value: 6, description: "√(7 + 9) + 2")
-
         brain.setOperand(7)
         brain.setOperation("+")
         brain.setOperand(9)
@@ -76,41 +76,44 @@ class CalculatorBrainTest: XCTestCase {
         brain.setOperand(2)
         brain.setOperation("=")
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        let expected: Expectation = (value: 6, description: "√(7 + 9) + 2")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
     }
 
     // Task 7f
     func testUnaryWithinPendingBinaryOperation() {
-        let expected: Expectation = (value: 3, description: "7 + √(9)")
-
         brain.setOperand(7)
         brain.setOperation("+")
         brain.setOperand(9)
         brain.setOperation("√")
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        let expected: Expectation = (value: 3, description: "7 + √(9)")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
     }
 
     // Task 7g
     func testUnaryWithinBinaryOperation() {
-        let expected: Expectation = (value: 10, description: "7 + √(9)")
-
         brain.setOperand(7)
         brain.setOperation("+")
         brain.setOperand(9)
         brain.setOperation("√")
         brain.setOperation("=")
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        let expected: Expectation = (value: 10, description: "7 + √(9)")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
     }
 
     // Task 7h
     func testMultipleSequentialBinaryOperations() {
-        let expected: Expectation = (value: 25, description: "7 + 9 + 6 + 3")
-
         brain.setOperand(7)
         brain.setOperation("+")
         brain.setOperand(9)
@@ -122,13 +125,17 @@ class CalculatorBrainTest: XCTestCase {
         brain.setOperand(3)
         brain.setOperation("=")
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        let expected: Expectation = (value: 25, description: "7 + 9 + 6 + 3")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
     }
 
     // Task 7i
     func testStartNewDescriptionAfterCompletedOperation() {
         let expected: Expectation = (value: 9, description: "6 + 3")
+        let actual = brain.evaluate()
 
         brain.setOperand(7)
         brain.setOperation("+")
@@ -140,29 +147,28 @@ class CalculatorBrainTest: XCTestCase {
         brain.setOperand(3)
         brain.setOperation("=")
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
     }
 
     // Task 7j is tested in InfixCalculatorUITests.swift
 
     // Task 7k
     func testBinaryOperationWithConstant() {
-        let expected: Expectation = (value: 12.5663706143592, description: "4 × π")
-
         brain.setOperand(4)
         brain.setOperation("×")
         brain.setOperation("π")
         brain.setOperation("=")
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        let expected: Expectation = (value: 12.5663706143592, description: "4 × π")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
     }
 
     // Hint 7
     func testConsecutiveBinaryOperations() {
-        let expected: Expectation = (value: 360, description: "6 × 5 × 4 × 3")
-
         brain.setOperand(6)
         brain.setOperation("×")
         brain.setOperand(5)
@@ -172,40 +178,43 @@ class CalculatorBrainTest: XCTestCase {
         brain.setOperand(3)
         brain.setOperation("=")
 
-        XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-        XCTAssertEqual(expected.description, brain.description)
+        let expected: Expectation = (value: 360, description: "6 × 5 × 4 × 3")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
     }
 
-    /* Order of operations is not required by the assignment and would add an extra layer of complexity
-     
-     func testHighAfterLowPrecedence() {
-     let expected: Expectation = (value: 8, description: "6 + 4 ÷ 2")
-     
-     brain.setOperand(6)
-     brain.setOperation("+")
-     brain.setOperand(4)
-     brain.performOperation("÷")
-     brain.setOperand(2)
-     brain.performOperation("=")
-     
-     XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-     XCTAssertEqual(expected.description, brain.description)
-     }
-     
-     func testHighAfterCompletedLowPrecedence() {
-     let expected: Expectation = (value: 5, description: "(6 + 4) ÷ 2")
-     
-     brain.setOperand(6)
-     brain.performOperation("+")
-     brain.setOperand(4)
-     brain.performOperation("=")
-     brain.performOperation("÷")
-     brain.setOperand(2)
-     brain.performOperation("=")
-     
-     XCTAssertEqualWithAccuracy(expected.value, brain.result!, accuracy: accuracy)
-     XCTAssertEqual(expected.description, brain.description)
-     }
-     
-     */
+    // Order of operations is not required by the assignment and would add an extra layer of complexity
+
+    /*func testHighAfterLowPrecedence() {
+        brain.setOperand(6)
+        brain.setOperation("+")
+        brain.setOperand(4)
+        brain.setOperation("÷")
+        brain.setOperand(2)
+        brain.setOperation("=")
+
+        let expected: Expectation = (value: 8, description: "6 + 4 ÷ 2")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
+    }
+
+    func testHighAfterCompletedLowPrecedence() {
+        brain.setOperand(6)
+        brain.setOperation("+")
+        brain.setOperand(4)
+        brain.setOperation("=")
+        brain.setOperation("÷")
+        brain.setOperand(2)
+        brain.setOperation("=")
+
+        let expected: Expectation = (value: 5, description: "(6 + 4) ÷ 2")
+        let actual = brain.evaluate()
+
+        XCTAssertEqualWithAccuracy(actual.result!, expected.value, accuracy: accuracy)
+        XCTAssertEqual(actual.description, expected.description)
+    }*/
 }
